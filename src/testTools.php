3c97efd904d4442b943ff61ab92371a168f3e573
michael <michael@cadot.info>
2021-11-13 20:15:01 +0000

    remove void and mixed

--- src/testTools.php
+++ src/testTools.php
@@ -49,6 +49,7 @@ trait testTools
      * @param  int $level
      * @param  array $opts option for getLinks
      * @param  mixed $liens for recurivity
+     * @return void
      */
     function getall($client, string $url, int $level = 0, $opts = [], $liens = [])
     {
